8e36b0c7f448d73e446184c888f4d946946e2fde
Bob Krzaczek <Robert.Krzaczek@gmail.com>
2018-07-01 18:53:48 -0400

    Added AWHEN macro to misc.lisp.

--- src/misc.lisp
+++ src/misc.lisp
@@ -1,19 +1,27 @@
 (uiop:define-package :accretions/src/misc
   (:use :common-lisp)
-  (:export #:strcat #:dlambda))
+  (:export #:strcat #:awhen #:dlambda))
 (in-package :accretions/src/misc)
 
 (defmacro strcat (&rest strings)
   `(concatenate 'string ,@strings))
 
+(defmacro awhen (test &body body)
+  "Anaphoric WHEN: Evaluates the TEST form once.  If true \(not NIL\),
+  BODY forms are then evaluated with the result of TEST bound to the
+  symbol IT."
+  `(let ((it ,test))
+     (when it
+       ,@body)))
+
 (defmacro dlambda (&rest dargs)
-  "A wrapper around LAMBDA that implements dispatch functionality on
-  its first argument.  This is based on Doug Hoyte's discussion and
-  implementation from his book \"Let Over Lambda,\" which is the best
-  reference for how to use this macro.  The only difference is that
-  the G!ARGS binding has been replaced with an explicit gensym bound
-  to ARGS \(thus saving us from other macros that would be necessary
-  to use this\)."
+  "A wrapper around LAMBDA that includes scaffolding supporting
+  dispatch functionality in the generated function's first argument.
+  This is based on Doug Hoyte's discussion and implementation from his
+  book \"Let Over Lambda,\" which is the best reference for how to use
+  this macro.  The only difference is that the G!ARGS binding has been
+  replaced with an explicit gensym bound to ARGS \(thus saving us from
+  other macros that would be necessary to use this\)."
   (let ((args (gensym)))
     `(lambda (&rest ,args)
        (case (car ,args)
